8b1c006a82289a5a978dfbb36996c02a37074da9
Brandon <brandonnguyenlam@gmail.com>
2023-04-22 15:54:28 -0500

    😒 third github repo lol

--- src/customBlocks/custom_Blocks.js
+++ src/customBlocks/custom_Blocks.js
@@ -1,43 +1,42 @@
-import Blockly from 'blockly';
+import Blockly from "blockly";
 import 'blockly/python';
 
-Blockly.Blocks['new_boundary_function'] = {
-    init: function () {
+Blockly.Blocks['start_selenium'] = {
+    init: function() {
         this.appendDummyInput()
-            .appendField(new Blockly.FieldTextInput("Boundary Function Name"), "Name");
-        this.appendStatementInput("Content")
-            .setCheck(null);
-        this.setInputsInline(true);
-        this.setColour(315);
+            .appendField("Start Selenium");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(120);
         this.setTooltip("");
         this.setHelpUrl("");
     }
 };
 
-Blockly.Python['new_boundary_function'] = function (block) {
-    var text_name = block.getFieldValue('Name');
-    var statements_content = Blockly.Python.statementToCode(block, 'Content');
-    // TODO: Assemble Python into code variable.
-    var code = 'def ' + text_name + '(_object,**kwargs):\n' + statements_content + '\n';
+Blockly.Blocks['go_to_link'] = {
+    init: function() {
+        this.appendValueInput("url")
+            .setCheck("String")
+            .setAlign(Blockly.ALIGN_RIGHT)
+            .appendField("Go to URL");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Python['start_selenium'] = function(block) {
+    var code = 'from selenium import webdriver\n';
+    code += 'from selenium.webdriver.common.keys import Keys\n';
+    code += 'from selenium.webdriver.common.by import By\n';
+    code += 'driver = webdriver.Chrome()\n';
     return code;
 };
 
-Blockly.Blocks['return'] = {
-    init: function () {
-        this.appendValueInput("NAME")
-            .setCheck(null)
-            .appendField("return");
-        this.setInputsInline(false);
-        this.setPreviousStatement(true, null);
-        this.setColour(330);
-        this.setTooltip("");
-        this.setHelpUrl("");
-    }
-};
-
-Blockly.Python['return'] = function (block) {
-    var value_name = Blockly.Python.valueToCode(block, 'NAME', Blockly.Python.ORDER_ATOMIC);
-    // TODO: Assemble Python into code variable.
-    var code = 'return ' + value_name + '\n';
+Blockly.Python['go_to_link'] = function(block) {
+    var value_url = Blockly.Python.valueToCode(block, 'url', Blockly.Python.ORDER_ATOMIC);
+    var code = 'driver.get(' + value_url + ')\n';
     return code;
 };
