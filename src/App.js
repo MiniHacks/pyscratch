b4eb36f0e78561e2338b8e4250ad5582783fdd00
Ansh Patel <pate1011@umn.edu>
2023-04-22 20:57:57 -0700

    🫳CHANGED STARTING CODE

--- src/App.js
+++ src/App.js
@@ -3,63 +3,63 @@ import "./customBlocks/custom_Blocks";
 import React, {useEffect, useState} from "react";
 import { BlocklyWorkspace } from "react-blockly";
 import Blockly from "blockly";
-import "blockly/python";
+import pythonGenerator from "blockly/python";
 import {toolboxCategories} from "./toolboxCategories";
 
-
-
 export default function App() {
-  const [xml, setXml] = useState("");
-  const [pythonCode, setPythonCode] = useState("");
-  useEffect(() =>{
+    const [xml, setXml] = useState("");
+    const [pythonCode, setPythonCode] = useState("");
+    useEffect(() =>{
 
-  }, [])
-  const initialXml =
-      '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" x="70" y="30"><field name="TEXT"></field></block></xml>';
-  function workspaceDidChange(workspace) {
-    const code = Blockly.Python.workspaceToCode(workspace);
-    setPythonCode(code);
-  }
+    }, [])
+    const initialXml =
+        '<xml xmlns="https://developers.google.com/blockly/xml"><block type="start_selenium" id="aKUkJCFO_I|nYt1W},No" x="77" y="56"><next><block type="go_to_link" id="Wc1[Q@7gDjjL8BL_e5us"><value name="url"><block type="text" id="9vtGi1i98.}7[,]HAYkG"><field name="TEXT">https://zoom.us/</field></block></value><next><block type="click_button_text" id="oOsG3_,G%=/]PsUERU}-"><value name="Text"><block type="text" id="ww-mT8if%blm^lhR1LA^"><field name="TEXT">Sign Up</field></block></value></block></next></block></next></block><block type="fill_input" id="e:pck@4B.5Sr[.M3o3.L" x="77" y="206"><value name="input"><block type="text" id="|$`TllsDj%.{{k`gEAJC"><field name="TEXT">email</field></block></value><value name="NAME"><block type="text" id="$UT[W=`$$4uA)3+`[Rf3"><field name="TEXT">//*[@id="email"]</field></block></value></block><block type="fill_input" id="W}c_+Jn+4#18~[ec5cyN" x="80" y="334"><value name="input"><block type="text" id="bYH}GnzjT9t#?Gm)sf!}"><field name="TEXT">password</field></block></value><value name="NAME"><block type="text" id="URb-;xk)v+8W__g=zR81"><field name="TEXT">//*[@id="password"]</field></block></value></block></xml>'
+    function workspaceDidChange(workspace) {
+        const code = pythonGenerator.workspaceToCode(workspace);
+        setPythonCode(code);
+    }
 
-  return (
-      <>
-        <div className="BG">
-          <div>
-            <div className="title">pyscratch</div>
-            <div className="Slogan">Scratch For <br/> Adults</div>
-            <div className="Subtitle">A cheesy line that's pog</div>
-            <div className="GSButton">Get Started</div>
-          </div>
+    return (
+        <>
+            <div className="BG">
+                <div>
+                    <div className="title">pyscratch</div>
+                    <div className="Slogan">Scratch For <br/> Adults</div>
+                    <div className="Subtitle">A cheesy line that's pog</div>
+                    <div className="GSButton">Get Started</div>
+                </div>
 
-        </div>
-          <div style={{
-              width: "100%",
-              height: "200px",
-              backgroundColor: "#1e1e1e",
-              border: "1px solid #1e1e1e",
-          }}>
-          </div>
-          <div style={{
-            display: "flex",
-            flexDirection: "row",
-          }}>
-        <BlocklyWorkspace
-            toolboxConfiguration={toolboxCategories}
-            initialXml={initialXml}
-            className="fill-height"
-            workspaceConfiguration={{
-              theme: "dark"
-            }}
-            onWorkspaceChange={workspaceDidChange}
-            onXmlChange={setXml}
-        />
-        <textarea
-            id="code"
-            style={{ height: "700px", width: "50%", backgroundColor: "#1e1e1e", color: "white"}}
-            value={pythonCode}
-            readOnly
-        ></textarea>
-          </div>
-      </>
-  );
+            </div>
+            <div style={{
+                width: "100%",
+                height: "200px",
+                backgroundColor: "#1e1e1e",
+                border: "1px solid #1e1e1e",
+            }}>
+            </div>
+            <div style={{
+                display: "flex",
+                flexDirection: "row",
+            }}>
+                <BlocklyWorkspace
+                    toolboxConfiguration={toolboxCategories}
+                    initialXml={initialXml}
+                    className="fill-height"
+                    workspaceConfiguration={{
+                        theme: "dark"
+                    }}
+                    onWorkspaceChange={workspaceDidChange}
+                    onXmlChange={setXml}
+                />
+
+                <textarea
+                    id="code"
+                    style={{ height: "700px", width: "50%", backgroundColor: "#1e1e1e", color: "white"}}
+                    value={pythonCode}
+                    readOnly
+                ></textarea>
+            </div>
+
+        </>
+    );
 }
