6eb69bfb49d5a21cb48287450cc1f5714b61f063
Ansh Patel <pate1011@umn.edu>
2023-04-22 22:14:06 -0700

    🫳some basic frontend changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BlocklyWorkspace } from "react-blockly";
 import Blockly from "blockly";
 import pythonGenerator from "blockly/python";
 import {toolboxCategories} from "./toolboxCategories";
+import Image from "./ImageForLandingPage.png";
 
 export default function App() {
     const [xml, setXml] = useState("");
@@ -13,27 +14,32 @@ export default function App() {
 
     }, [])
     const initialXml =
-        '<xml xmlns="https://developers.google.com/blockly/xml"><block type="start_selenium" id="aKUkJCFO_I|nYt1W},No" x="77" y="56"><next><block type="go_to_link" id="Wc1[Q@7gDjjL8BL_e5us"><value name="url"><block type="text" id="9vtGi1i98.}7[,]HAYkG"><field name="TEXT">https://zoom.us/</field></block></value><next><block type="click_button_text" id="oOsG3_,G%=/]PsUERU}-"><value name="Text"><block type="text" id="ww-mT8if%blm^lhR1LA^"><field name="TEXT">Sign Up</field></block></value></block></next></block></next></block><block type="fill_input" id="e:pck@4B.5Sr[.M3o3.L" x="77" y="206"><value name="input"><block type="text" id="|$`TllsDj%.{{k`gEAJC"><field name="TEXT">email</field></block></value><value name="NAME"><block type="text" id="$UT[W=`$$4uA)3+`[Rf3"><field name="TEXT">//*[@id="email"]</field></block></value></block><block type="fill_input" id="W}c_+Jn+4#18~[ec5cyN" x="80" y="334"><value name="input"><block type="text" id="bYH}GnzjT9t#?Gm)sf!}"><field name="TEXT">password</field></block></value><value name="NAME"><block type="text" id="URb-;xk)v+8W__g=zR81"><field name="TEXT">//*[@id="password"]</field></block></value></block></xml>'
+        '<xml xmlns="https://developers.google.com/blockly/xml"><block type="start_selenium" id="6}?:zoYRm3^d??T5//+H" x="73" y="27"><next><block type="import_pd_and_np" id="M%/Zs;4@YZh/-H}~/FP%"><next><block type="go_to_link" id="e~x7NH$WpNRrqKS/o(8V"><value name="url"><block type="text" id="*N}S]fAE:(sWnR66HhtN"><field name="TEXT">https://hoopshype.com/salaries/players/</field></block></value><next><block type="scrape_element_class_name" id="QUJ/j@X.#qMwe9ziH#rz"><value name="name"><block type="text" id="xR$8O2m`0u%N`$TS~v[Z"><field name="TEXT">Name</field></block></value><value name="class_name"><block type="text" id="*s*j[q,a)~LIeYV/qk5y"><field name="TEXT">name</field></block></value><next><block type="scrape_element_class_name" id="snM|Zkdp]G.4%{RSPAkh"><value name="name"><block type="text" id="w~=(2~C^KK;f(jqUKHNu"><field name="TEXT">Salary</field></block></value><value name="class_name"><block type="text" id="76CtNpeaG^dx790mh4{B"><field name="TEXT">hh-salaries-sorted</field></block></value><next><block type="generate_csv" id="7il}[PFMWAhUBXw!5-,#"><value name="name"><block type="text" id="/musb3Z/kU7F$?lPbh^."><field name="TEXT">example.csv</field></block></value></block></next></block></next></block></next></block></next></block></next></block></xml>';
     function workspaceDidChange(workspace) {
         const code = pythonGenerator.workspaceToCode(workspace);
         setPythonCode(code);
     }
-
     return (
         <>
             <div className="BG">
-                <div>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                }
+                }>
                     <div className="title">pyscratch</div>
                     <div className="Slogan">Scratch For <br/> Adults</div>
                     <div className="Subtitle">A cheesy line that's pog</div>
                     <div className="GSButton">Get Started</div>
                 </div>
-
+                <div className="Image">
+                    <img src={Image} alt="Image" width="700px" height="800px"/>
+                </div>
             </div>
             <div style={{
                 width: "100%",
-                height: "200px",
-                backgroundColor: "#1e1e1e",
+                height: "800px",
+                backgroundColor: "#fff",
                 border: "1px solid #1e1e1e",
             }}>
             </div>
@@ -59,7 +65,6 @@ export default function App() {
                     readOnly
                 ></textarea>
             </div>
-
         </>
     );
 }
